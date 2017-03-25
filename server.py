1a27b595ca40ceed5f30e39a750f96606c313962
Leon Liang <llian008@fiu.edu>
2017-03-25 17:41:33 -0400

    segmented pet_finder api call

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from pet_finder import *
 import requests
 import hashlib
 import petfinder
@@ -11,18 +12,7 @@ def imageUpload():
 #work in progress
 @app.route("/petFinder")
 def petFinderAPI():
-    apiKey = "a3c7ab98952cd4109db691674901ac59"
-    apiSecret="dc72e05d6cb02cbed2aff439111eea77"
-    api = petfinder.PetFinderClient(api_key=apiKey, api_secret=apiSecret)
-    petDict = []
-    for pet in api.pet_find(
-        animal="dog", location="33193", output="basic",
-        breed="Labrador", count=1
-    ):
-        print(pet['name'], pet['id'])
-
-
-    return petDict
+    petFinder();
 
 
 if __name__ == "__main__":
